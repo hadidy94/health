46b90f890ec590e9a3af85e5b4ddba7902214abd
hadidy94 <ahmedhadidy94@gmail.com>
2022-09-17 13:18:07 +0300

    ss

--- src/libs/js/functions.js
+++ src/libs/js/functions.js
@@ -1,14 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
-      everydropdown.addEventListener('shown.bs.dropdown', function () {
-          el_overlay = document.createElement('span');
-          el_overlay.className = 'screen-darken';
-          document.getElementById('main-cont').appendChild(el_overlay)
-      });
-  
-      everydropdown.addEventListener('hide.bs.dropdown', function () {
-        document.getElementById('main-cont').removeChild(document.querySelector('.screen-darken'));
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('.navbar .dropdown').forEach(function (everydropdown) {
+    everydropdown.addEventListener('shown.bs.dropdown', function () {
+      el_overlay = document.createElement('span');
+      el_overlay.className = 'screen-darken';
+      document.getElementById('main-cont').appendChild(el_overlay)
     });
-  
-  }); 
+
+    everydropdown.addEventListener('hide.bs.dropdown', function () {
+      document.getElementById('main-cont').removeChild(document.querySelector('.screen-darken'));
+    });
+  });
+
+  $(document).ready(function ($) {
+    $(".table-row").click(function () {
+      window.document.location = $(this).data("href");
+    });
+  });
+
+
+}); 
